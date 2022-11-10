a31f2371ab267472a94e9597d976bb60defa82d3
Mr-Cool-in-Git <kylin98@list.ru>
2022-11-10 15:01:55 +0300

    123

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ import pandas as pd
 # h.update(b)
 # print(h.hexdigest())
 # #
-# response = requests.get(f'http://127.0.0.1:8001/clients/auth?login=nick&password=12314')
+# response = requests.get(f'http://https://mrcool-greenbank.herokuapp.com/clients/auth?login=nick&password=12314')
 # data = response.json()
 #
 data = pd.DataFrame(columns=('number', 'type', 'amount'))
